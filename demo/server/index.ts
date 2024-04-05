ba0ae55f1282a521fa153119a16433c1cf6a1c6c
Mark Krasner <mzkrasner@gmail.com>
2024-04-05 11:07:02 -0400

    Restricting origin in cors

--- demo/server/index.ts
+++ demo/server/index.ts
@@ -6,8 +6,14 @@ import multiRouter from "./routes/multi";
 const app = express();
 const port = 8080; // default port to listen
 
+
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://developers.ceramic.network"],
+  optionsSuccessStatus: 200 // For legacy browser support
+  }
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions)); 
 // app.use(bodyParser.json());
 
 const allowCrossDomain = (
