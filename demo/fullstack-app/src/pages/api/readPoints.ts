8999bd36d8d22a71f637248809a82ccf7e992630
Mark Krasner <mzkrasner@gmail.com>
2024-04-24 09:44:43 -0400

    Simplifying readPoints api

--- demo/fullstack-app/src/pages/api/readPoints.ts
+++ demo/fullstack-app/src/pages/api/readPoints.ts
@@ -19,26 +19,11 @@ export default async function handler(req: Request, res: Response) {
   try {
     const { recipient, context } = req.body
 
-    // get context aggregation doc if exists
-    const contextAggregationDoc: ModelInstanceDocument<AggregationContent> | null =
-      await contextReader.loadAggregationDocumentFor([recipient, context])
-
-    // get total aggregation doc if exists
-    const totalAggregationDoc: ModelInstanceDocument<AggregationContent> | null =
-      await reader.loadAggregationDocumentFor([recipient])
-
-    res.status(200).send({
-      contextTotal: contextAggregationDoc
-        ? contextAggregationDoc.content
-          ? contextAggregationDoc.content.points
-          : 0
-        : 0,
-      total: totalAggregationDoc
-        ? totalAggregationDoc.content
-          ? totalAggregationDoc.content.points
-          : 0
-        : 0,
-    })
+    const [contextTotal, total] = await Promise.all([
+      contextReader.getAggregationPointsFor([recipient, context]),
+      reader.getAggregationPointsFor([recipient]),
+    ])
+    res.status(200).send({ contextTotal, total })
   } catch (error) {
     console.error(error)
     res.status(500).send({ error: 'Internal Server Error' })
